2afbc7f8435aeb4d312928ab867350a3187ef046
havvtom <havvtom@gmail.com>
2019-10-10 15:32:06 +0200

    corrected css path

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,13 +8,33 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/app.css')}}" >
     </head>
     <body>
         <div id="app">
 
-            <router-link to="/">Home</router-link>
+            <!-- <router-link to="/">Home</router-link>
             <router-link to="/about">About</router-link>
-            <router-link to="/contactUs">Contact Us</router-link>
+            <router-link to="/contactUs">Contact Us</router-link> -->
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <a class="navbar-brand" href="#">Navbar</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <router-link class="nav-item nav-link" to="/">Home</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="nav-item nav-link" to="/about">About</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="nav-item nav-link" to="/contactUs">Contact Us</router-link>
+                  </li>                  
+                </ul>
+              </div>
+            </nav>
 
             <router-view></router-view>
             
